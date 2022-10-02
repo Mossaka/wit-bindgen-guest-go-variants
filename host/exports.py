830739961d311dfc97ce69e76ffa2e004c67ac82
Jiaxiao Zhou <jiazho@microsoft.com>
2022-10-02 00:13:16 -0700

    generated host
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- host/exports.py
+++ host/exports.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod
 import ctypes
-from typing import Any, List, Tuple, cast
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any, Generic, Optional, TypeVar, Union
 import wasmtime
 
 try:
@@ -9,6 +11,12 @@ except ImportError:
     class Protocol: # type: ignore
         pass
 
+T = TypeVar('T')
+
+def _clamp(i: int, min: int, max: int) -> int:
+    if i < min or i > max:
+        raise OverflowError(f'must be between {min} and {max}')
+    return i
 
 def _load(ty: Any, mem: wasmtime.Memory, store: wasmtime.Storelike, base: int, offset: int) -> Any:
     ptr = (base & 0xffffffff) + offset
@@ -20,132 +28,144 @@ def _load(ty: Any, mem: wasmtime.Memory, store: wasmtime.Storelike, base: int, o
     )
     return c_ptr[0]
 
-def _decode_utf8(mem: wasmtime.Memory, store: wasmtime.Storelike, ptr: int, len: int) -> str:
-    ptr = ptr & 0xffffffff
-    len = len & 0xffffffff
-    if ptr + len > mem.data_len(store):
-        raise IndexError('string out of bounds')
-    base = mem.data_ptr(store)
-    base = ctypes.POINTER(ctypes.c_ubyte)(
-        ctypes.c_ubyte.from_address(ctypes.addressof(base.contents) + ptr)
-    )
-    return ctypes.string_at(base, len).decode('utf-8')
+@dataclass
+class Ok(Generic[T]):
+    value: T
+E = TypeVar('E')
+@dataclass
+class Err(Generic[E]):
+    value: E
 
-def _encode_utf8(val: str, realloc: wasmtime.Func, mem: wasmtime.Memory, store: wasmtime.Storelike) -> Tuple[int, int]:
-    bytes = val.encode('utf8')
-    ptr = realloc(store, 0, 0, 1, len(bytes))
-    assert(isinstance(ptr, int))
-    ptr = ptr & 0xffffffff
-    if ptr + len(bytes) > mem.data_len(store):
-        raise IndexError('string out of bounds')
-    base = mem.data_ptr(store)
-    base = ctypes.POINTER(ctypes.c_ubyte)(
-        ctypes.c_ubyte.from_address(ctypes.addressof(base.contents) + ptr)
-    )
-    ctypes.memmove(base, bytes, len(bytes))
-    return (ptr, len(bytes))
+Result = Union[Ok[T], Err[E]]
+_i32_to_f32_i32 = ctypes.pointer(ctypes.c_int32(0))
+_i32_to_f32_f32 = ctypes.cast(_i32_to_f32_i32, ctypes.POINTER(ctypes.c_float))
 
-def _list_canon_lift(ptr: int, len: int, size: int, ty: Any, mem: wasmtime.Memory ,store: wasmtime.Storelike) -> Any:
-    ptr = ptr & 0xffffffff
-    len = len & 0xffffffff
-    if ptr + len * size > mem.data_len(store):
-        raise IndexError('list out of bounds')
-    raw_base = mem.data_ptr(store)
-    base = ctypes.POINTER(ty)(
-        ty.from_address(ctypes.addressof(raw_base.contents) + ptr)
-    )
-    if ty == ctypes.c_uint8:
-        return ctypes.string_at(base, len)
-    return base[:len]
+def _f32_to_i32(i: float) -> int:
+    _i32_to_f32_f32[0] = i    # type: ignore
+    return _i32_to_f32_i32[0] # type: ignore
+class E1(Enum):
+    A = 0
+    B = 1
 
-def _list_canon_lower(list: Any, ty: Any, size: int, align: int, realloc: wasmtime.Func, mem: wasmtime.Memory, store: wasmtime.Storelike) -> Tuple[int, int]:
-    total_size = size * len(list)
-    ptr = realloc(store, 0, 0, align, total_size)
-    assert(isinstance(ptr, int))
-    ptr = ptr & 0xffffffff
-    if ptr + total_size > mem.data_len(store):
-        raise IndexError('list realloc return of bounds')
-    raw_base = mem.data_ptr(store)
-    base = ctypes.POINTER(ty)(
-        ty.from_address(ctypes.addressof(raw_base.contents) + ptr)
-    )
-    for i, val in enumerate(list):
-        base[i] = val
-    return (ptr, len(list))
+@dataclass
+class C1A:
+    value: int
+
+@dataclass
+class C1B:
+    value: int
+
+C1 = Union[C1A, C1B]
+
 class Exports:
     instance: wasmtime.Instance
-    _allocated_bytes: wasmtime.Func
-    _cabi_post_list_roundtrip: wasmtime.Func
-    _cabi_post_string_roundtrip: wasmtime.Func
-    _cabi_realloc: wasmtime.Func
-    _list_roundtrip: wasmtime.Func
     _memory: wasmtime.Memory
-    _string_roundtrip: wasmtime.Func
+    _roundtrip_enum: wasmtime.Func
+    _roundtrip_option: wasmtime.Func
+    _roundtrip_result: wasmtime.Func
     _test_imports: wasmtime.Func
+    _variant_c1: wasmtime.Func
     def __init__(self, store: wasmtime.Store, linker: wasmtime.Linker, module: wasmtime.Module):
         self.instance = linker.instantiate(store, module)
         exports = self.instance.exports(store)
-        
-        allocated_bytes = exports['allocated-bytes: func() -> u32']
-        assert(isinstance(allocated_bytes, wasmtime.Func))
-        self._allocated_bytes = allocated_bytes
-        
-        cabi_post_list_roundtrip = exports['cabi_post_list-roundtrip']
-        assert(isinstance(cabi_post_list_roundtrip, wasmtime.Func))
-        self._cabi_post_list_roundtrip = cabi_post_list_roundtrip
-        
-        cabi_post_string_roundtrip = exports['cabi_post_string-roundtrip']
-        assert(isinstance(cabi_post_string_roundtrip, wasmtime.Func))
-        self._cabi_post_string_roundtrip = cabi_post_string_roundtrip
-        
-        cabi_realloc = exports['cabi_realloc']
-        assert(isinstance(cabi_realloc, wasmtime.Func))
-        self._cabi_realloc = cabi_realloc
-        
-        list_roundtrip = exports['list-roundtrip: func(a: list<u8>) -> list<u8>']
-        assert(isinstance(list_roundtrip, wasmtime.Func))
-        self._list_roundtrip = list_roundtrip
         
         memory = exports['memory']
         assert(isinstance(memory, wasmtime.Memory))
         self._memory = memory
         
-        string_roundtrip = exports['string-roundtrip: func(a: string) -> string']
-        assert(isinstance(string_roundtrip, wasmtime.Func))
-        self._string_roundtrip = string_roundtrip
+        roundtrip_enum = exports['roundtrip-enum: func(a: enum { a, b }) -> enum { a, b }']
+        assert(isinstance(roundtrip_enum, wasmtime.Func))
+        self._roundtrip_enum = roundtrip_enum
+        
+        roundtrip_option = exports['roundtrip-option: func(a: option<float32>) -> option<u8>']
+        assert(isinstance(roundtrip_option, wasmtime.Func))
+        self._roundtrip_option = roundtrip_option
+        
+        roundtrip_result = exports['roundtrip-result: func(a: result<u32, float32>) -> result<float64, u8>']
+        assert(isinstance(roundtrip_result, wasmtime.Func))
+        self._roundtrip_result = roundtrip_result
         
         test_imports = exports['test-imports: func() -> ()']
         assert(isinstance(test_imports, wasmtime.Func))
         self._test_imports = test_imports
+        
+        variant_c1 = exports['variant-c1: func(a: variant { a(s32), b(s64) }) -> variant { a(s32), b(s64) }']
+        assert(isinstance(variant_c1, wasmtime.Func))
+        self._variant_c1 = variant_c1
     def test_imports(self, caller: wasmtime.Store) -> None:
         self._test_imports(caller)
-    def allocated_bytes(self, caller: wasmtime.Store) -> int:
-        ret = self._allocated_bytes(caller)
-        assert(isinstance(ret, int))
-        return ret & 0xffffffff
-    def list_roundtrip(self, caller: wasmtime.Store, a: bytes) -> bytes:
+    def roundtrip_option(self, caller: wasmtime.Store, a: Optional[float]) -> Optional[int]:
         memory = self._memory;
-        realloc = self._cabi_realloc
-        ptr, len0 = _list_canon_lower(a, ctypes.c_uint8, 1, 1, realloc, memory, caller)
-        ret = self._list_roundtrip(caller, ptr, len0)
+        if a is None:
+            variant = 0
+            variant1 = 0.0
+        else:
+            payload0 = a
+            variant = 1
+            variant1 = payload0
+        ret = self._roundtrip_option(caller, variant, variant1)
         assert(isinstance(ret, int))
-        load = _load(ctypes.c_int32, memory, caller, ret, 0)
-        load1 = _load(ctypes.c_int32, memory, caller, ret, 4)
-        ptr2 = load
-        len3 = load1
-        list = cast(bytes, _list_canon_lift(ptr2, len3, 1, ctypes.c_uint8, memory, caller))
-        self._cabi_post_list_roundtrip(caller, ret)
-        return list
-    def string_roundtrip(self, caller: wasmtime.Store, a: str) -> str:
+        load = _load(ctypes.c_uint8, memory, caller, ret, 0)
+        option: Optional[int]
+        if load == 0:
+            option = None
+        elif load == 1:
+            load2 = _load(ctypes.c_uint8, memory, caller, ret, 1)
+            option = _clamp(load2, 0, 255)
+        else:
+            raise TypeError("invalid variant discriminant for option")
+        return option
+    def roundtrip_result(self, caller: wasmtime.Store, a: Result[int, float]) -> Result[float, int]:
         memory = self._memory;
-        realloc = self._cabi_realloc
-        ptr, len0 = _encode_utf8(a, realloc, memory, caller)
-        ret = self._string_roundtrip(caller, ptr, len0)
+        if isinstance(a, Ok):
+            payload = a.value
+            variant = 0
+            variant1 = _clamp(payload, 0, 4294967295)
+        elif isinstance(a, Err):
+            payload0 = a.value
+            variant = 1
+            variant1 = _f32_to_i32(payload0)
+        else:
+            raise TypeError("invalid variant specified for expected")
+        ret = self._roundtrip_result(caller, variant, variant1)
         assert(isinstance(ret, int))
-        load = _load(ctypes.c_int32, memory, caller, ret, 0)
-        load1 = _load(ctypes.c_int32, memory, caller, ret, 4)
-        ptr2 = load
-        len3 = load1
-        list = _decode_utf8(memory, caller, ptr2, len3)
-        self._cabi_post_string_roundtrip(caller, ret)
-        return list
+        load = _load(ctypes.c_uint8, memory, caller, ret, 0)
+        expected: Result[float, int]
+        if load == 0:
+            load2 = _load(ctypes.c_double, memory, caller, ret, 8)
+            expected = Ok(load2)
+        elif load == 1:
+            load3 = _load(ctypes.c_uint8, memory, caller, ret, 8)
+            expected = Err(_clamp(load3, 0, 255))
+        else:
+            raise TypeError("invalid variant discriminant for expected")
+        return expected
+    def roundtrip_enum(self, caller: wasmtime.Store, a: E1) -> E1:
+        ret = self._roundtrip_enum(caller, (a).value)
+        assert(isinstance(ret, int))
+        return E1(ret)
+    def variant_c1(self, caller: wasmtime.Store, a: C1) -> C1:
+        memory = self._memory;
+        if isinstance(a, C1A):
+            payload = a.value
+            variant = 0
+            variant1 = _clamp(payload, -2147483648, 2147483647)
+        elif isinstance(a, C1B):
+            payload0 = a.value
+            variant = 1
+            variant1 = _clamp(payload0, -9223372036854775808, 9223372036854775807)
+        else:
+            raise TypeError("invalid variant specified for C1")
+        ret = self._variant_c1(caller, variant, variant1)
+        assert(isinstance(ret, int))
+        load = _load(ctypes.c_uint8, memory, caller, ret, 0)
+        variant4: C1
+        if load == 0:
+            load2 = _load(ctypes.c_int32, memory, caller, ret, 8)
+            variant4 = C1A(load2)
+        elif load == 1:
+            load3 = _load(ctypes.c_int64, memory, caller, ret, 8)
+            variant4 = C1B(load3)
+        else:
+            raise TypeError("invalid variant discriminant for C1")
+        return variant4
--- host/imports.py
+++ host/imports.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod
 import ctypes
-from typing import Any, List, Tuple, cast
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any, Generic, Optional, TypeVar, Union, cast
 import wasmtime
 
 try:
@@ -9,6 +11,12 @@ except ImportError:
     class Protocol: # type: ignore
         pass
 
+T = TypeVar('T')
+
+def _clamp(i: int, min: int, max: int) -> int:
+    if i < min or i > max:
+        raise OverflowError(f'must be between {min} and {max}')
+    return i
 
 def _store(ty: Any, mem: wasmtime.Memory, store: wasmtime.Storelike, base: int, offset: int, val: Any) -> None:
     ptr = (base & 0xffffffff) + offset
@@ -20,94 +28,120 @@ def _store(ty: Any, mem: wasmtime.Memory, store: wasmtime.Storelike, base: int, 
     )
     c_ptr[0] = val
 
-def _decode_utf8(mem: wasmtime.Memory, store: wasmtime.Storelike, ptr: int, len: int) -> str:
-    ptr = ptr & 0xffffffff
-    len = len & 0xffffffff
-    if ptr + len > mem.data_len(store):
-        raise IndexError('string out of bounds')
-    base = mem.data_ptr(store)
-    base = ctypes.POINTER(ctypes.c_ubyte)(
-        ctypes.c_ubyte.from_address(ctypes.addressof(base.contents) + ptr)
-    )
-    return ctypes.string_at(base, len).decode('utf-8')
+@dataclass
+class Ok(Generic[T]):
+    value: T
+E = TypeVar('E')
+@dataclass
+class Err(Generic[E]):
+    value: E
 
-def _encode_utf8(val: str, realloc: wasmtime.Func, mem: wasmtime.Memory, store: wasmtime.Storelike) -> Tuple[int, int]:
-    bytes = val.encode('utf8')
-    ptr = realloc(store, 0, 0, 1, len(bytes))
-    assert(isinstance(ptr, int))
-    ptr = ptr & 0xffffffff
-    if ptr + len(bytes) > mem.data_len(store):
-        raise IndexError('string out of bounds')
-    base = mem.data_ptr(store)
-    base = ctypes.POINTER(ctypes.c_ubyte)(
-        ctypes.c_ubyte.from_address(ctypes.addressof(base.contents) + ptr)
-    )
-    ctypes.memmove(base, bytes, len(bytes))
-    return (ptr, len(bytes))
+Result = Union[Ok[T], Err[E]]
+_i32_to_f32_i32 = ctypes.pointer(ctypes.c_int32(0))
+_i32_to_f32_f32 = ctypes.cast(_i32_to_f32_i32, ctypes.POINTER(ctypes.c_float))
 
-def _list_canon_lift(ptr: int, len: int, size: int, ty: Any, mem: wasmtime.Memory ,store: wasmtime.Storelike) -> Any:
-    ptr = ptr & 0xffffffff
-    len = len & 0xffffffff
-    if ptr + len * size > mem.data_len(store):
-        raise IndexError('list out of bounds')
-    raw_base = mem.data_ptr(store)
-    base = ctypes.POINTER(ty)(
-        ty.from_address(ctypes.addressof(raw_base.contents) + ptr)
-    )
-    if ty == ctypes.c_uint8:
-        return ctypes.string_at(base, len)
-    return base[:len]
+def _i32_to_f32(i: int) -> float:
+    _i32_to_f32_i32[0] = i     # type: ignore
+    return _i32_to_f32_f32[0]  # type: ignore
+class E1(Enum):
+    A = 0
+    B = 1
 
-def _list_canon_lower(list: Any, ty: Any, size: int, align: int, realloc: wasmtime.Func, mem: wasmtime.Memory, store: wasmtime.Storelike) -> Tuple[int, int]:
-    total_size = size * len(list)
-    ptr = realloc(store, 0, 0, align, total_size)
-    assert(isinstance(ptr, int))
-    ptr = ptr & 0xffffffff
-    if ptr + total_size > mem.data_len(store):
-        raise IndexError('list realloc return of bounds')
-    raw_base = mem.data_ptr(store)
-    base = ctypes.POINTER(ty)(
-        ty.from_address(ctypes.addressof(raw_base.contents) + ptr)
-    )
-    for i, val in enumerate(list):
-        base[i] = val
-    return (ptr, len(list))
+@dataclass
+class C1A:
+    value: int
+
+@dataclass
+class C1B:
+    value: int
+
+C1 = Union[C1A, C1B]
+
 class Imports(Protocol):
     @abstractmethod
-    def list_roundtrip(self, a: bytes) -> bytes:
+    def roundtrip_option(self, a: Optional[float]) -> Optional[int]:
         raise NotImplementedError
     @abstractmethod
-    def string_roundtrip(self, a: str) -> str:
+    def roundtrip_result(self, a: Result[int, float]) -> Result[float, int]:
+        raise NotImplementedError
+    @abstractmethod
+    def roundtrip_enum(self, a: E1) -> E1:
+        raise NotImplementedError
+    @abstractmethod
+    def variant_c1(self, a: C1) -> C1:
         raise NotImplementedError
 
 def add_imports_to_linker(linker: wasmtime.Linker, store: wasmtime.Store, host: Imports) -> None:
-    ty = wasmtime.FuncType([wasmtime.ValType.i32(), wasmtime.ValType.i32(), wasmtime.ValType.i32()], [])
-    def list_roundtrip(caller: wasmtime.Caller, arg0: int, arg1: int, arg2: int) -> None:
+    ty = wasmtime.FuncType([wasmtime.ValType.i32(), wasmtime.ValType.f32(), wasmtime.ValType.i32()], [])
+    def roundtrip_option(caller: wasmtime.Caller, arg0: int, arg1: float, arg2: int) -> None:
         m = caller["memory"]
         assert(isinstance(m, wasmtime.Memory))
         memory = cast(wasmtime.Memory, m)
-        realloc = caller["cabi_realloc"]
-        assert(isinstance(realloc, wasmtime.Func))
-        ptr = arg0
-        len0 = arg1
-        list = cast(bytes, _list_canon_lift(ptr, len0, 1, ctypes.c_uint8, memory, caller))
-        ret = host.list_roundtrip(list)
-        ptr1, len2 = _list_canon_lower(ret, ctypes.c_uint8, 1, 1, realloc, memory, caller)
-        _store(ctypes.c_uint32, memory, caller, arg2, 4, len2)
-        _store(ctypes.c_uint32, memory, caller, arg2, 0, ptr1)
-    linker.define('imports', 'list-roundtrip: func(a: list<u8>) -> list<u8>', wasmtime.Func(store, ty, list_roundtrip, access_caller = True))
+        option: Optional[float]
+        if arg0 == 0:
+            option = None
+        elif arg0 == 1:
+            option = arg1
+        else:
+            raise TypeError("invalid variant discriminant for option")
+        ret = host.roundtrip_option(option)
+        if ret is None:
+            _store(ctypes.c_uint8, memory, caller, arg2, 0, 0)
+        else:
+            payload0 = ret
+            _store(ctypes.c_uint8, memory, caller, arg2, 0, 1)
+            _store(ctypes.c_uint8, memory, caller, arg2, 1, _clamp(payload0, 0, 255))
+    linker.define('imports', 'roundtrip-option: func(a: option<float32>) -> option<u8>', wasmtime.Func(store, ty, roundtrip_option, access_caller = True))
     ty = wasmtime.FuncType([wasmtime.ValType.i32(), wasmtime.ValType.i32(), wasmtime.ValType.i32()], [])
-    def string_roundtrip(caller: wasmtime.Caller, arg0: int, arg1: int, arg2: int) -> None:
+    def roundtrip_result(caller: wasmtime.Caller, arg0: int, arg1: int, arg2: int) -> None:
         m = caller["memory"]
         assert(isinstance(m, wasmtime.Memory))
         memory = cast(wasmtime.Memory, m)
-        realloc = caller["cabi_realloc"]
-        assert(isinstance(realloc, wasmtime.Func))
-        ptr = arg0
-        len0 = arg1
-        list = _decode_utf8(memory, caller, ptr, len0)
-        ret = host.string_roundtrip(list)
-        ptr1, len2 = _encode_utf8(ret, realloc, memory, caller)
-        _store(ctypes.c_uint32, memory, caller, arg2, 4, len2)
-        _store(ctypes.c_uint32, memory, caller, arg2, 0, ptr1)
-    linker.define('imports', 'string-roundtrip: func(a: string) -> string', wasmtime.Func(store, ty, string_roundtrip, access_caller = True))
+        expected: Result[int, float]
+        if arg0 == 0:
+            expected = Ok(arg1 & 0xffffffff)
+        elif arg0 == 1:
+            expected = Err(_i32_to_f32(arg1))
+        else:
+            raise TypeError("invalid variant discriminant for expected")
+        ret = host.roundtrip_result(expected)
+        if isinstance(ret, Ok):
+            payload = ret.value
+            _store(ctypes.c_uint8, memory, caller, arg2, 0, 0)
+            _store(ctypes.c_double, memory, caller, arg2, 8, payload)
+        elif isinstance(ret, Err):
+            payload0 = ret.value
+            _store(ctypes.c_uint8, memory, caller, arg2, 0, 1)
+            _store(ctypes.c_uint8, memory, caller, arg2, 8, _clamp(payload0, 0, 255))
+        else:
+            raise TypeError("invalid variant specified for expected")
+    linker.define('imports', 'roundtrip-result: func(a: result<u32, float32>) -> result<float64, u8>', wasmtime.Func(store, ty, roundtrip_result, access_caller = True))
+    ty = wasmtime.FuncType([wasmtime.ValType.i32()], [wasmtime.ValType.i32()])
+    def roundtrip_enum(caller: wasmtime.Caller, arg0: int) -> int:
+        ret = host.roundtrip_enum(E1(arg0))
+        return (ret).value
+    linker.define('imports', 'roundtrip-enum: func(a: enum { a, b }) -> enum { a, b }', wasmtime.Func(store, ty, roundtrip_enum, access_caller = True))
+    ty = wasmtime.FuncType([wasmtime.ValType.i32(), wasmtime.ValType.i64(), wasmtime.ValType.i32()], [])
+    def variant_c1(caller: wasmtime.Caller, arg0: int, arg1: int, arg2: int) -> None:
+        m = caller["memory"]
+        assert(isinstance(m, wasmtime.Memory))
+        memory = cast(wasmtime.Memory, m)
+        variant: C1
+        if arg0 == 0:
+            variant = C1A(arg1)
+        elif arg0 == 1:
+            variant = C1B(arg1)
+        else:
+            raise TypeError("invalid variant discriminant for C1")
+        ret = host.variant_c1(variant)
+        if isinstance(ret, C1A):
+            payload = ret.value
+            _store(ctypes.c_uint8, memory, caller, arg2, 0, 0)
+            _store(ctypes.c_uint32, memory, caller, arg2, 8, _clamp(payload, -2147483648, 2147483647))
+        elif isinstance(ret, C1B):
+            payload0 = ret.value
+            _store(ctypes.c_uint8, memory, caller, arg2, 0, 1)
+            _store(ctypes.c_uint64, memory, caller, arg2, 8, _clamp(payload0, -9223372036854775808, 9223372036854775807))
+        else:
+            raise TypeError("invalid variant specified for C1")
+    linker.define('imports', 'variant-c1: func(a: variant { a(s32), b(s64) }) -> variant { a(s32), b(s64) }', wasmtime.Func(store, ty, variant_c1, access_caller = True))
